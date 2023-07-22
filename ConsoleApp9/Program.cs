444154472386018f30ed17424a3d015ff5941d65
KornX_my68@student.itstep.org <KornX_my68@student.itstep.org>
2023-07-22 18:11:39 +0300

    1

--- ConsoleApp9/Program.cs
+++ ConsoleApp9/Program.cs
@@ -1,5 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("hi hitler2");
-Console.WriteLine("Я твою мать люблю");
-Console.WriteLine("Почему милфа ето классно?");
-//тьі не лох
+﻿
